1982ffeded39f79121f9e6d59d373a5877f82af5
Saidul Anam Siam <126325469+saidul-anam@users.noreply.github.com>
2025-06-14 01:27:29 +0600

    Update GameController.java

--- backend/src/main/java/com/chainreaction/controller/GameController.java
+++ backend/src/main/java/com/chainreaction/controller/GameController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.chainreaction.ai.MiniMaxService;
 import com.chainreaction.ai.Move;
